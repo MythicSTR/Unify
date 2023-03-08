d7f2f2a41b5a4b26ebbb74085603bb5d547983ca
Shishir Tamrakar <stamrakar545@gmail.com>
2023-03-08 11:25:58 +0545

    Setting up SQL database

--- backend/unify/settings.py
+++ backend/unify/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'unify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'unify',
+        'USER': 'root',
+        'PASSWORD': 'sqlpass123',
+        'PORT': 3306,
+        'HOST': '127.0.0.1',
     }
 }
 
