83594fda3c632c76ea1b4b27548921a25acec252
ankeet779 <100555111+ankeet779@users.noreply.github.com>
2023-05-09 10:53:36 +0545

    changes in StudentRoutine

--- frontend/src/components/StudentRoutine.js
+++ frontend/src/components/StudentRoutine.js
@@ -16,6 +16,7 @@ function StudentRoutine() {
   ];
 
   const periods = [
+    { start: "7:00 AM", end: "8:00 AM" },
     { start: "7:00 AM", end: "9:00 AM" },
     { start: "9:00 AM", end: "10:00 AM" },
     { start: "10:00 AM", end: "11:00 AM" },
@@ -136,3 +137,89 @@ function StudentRoutine() {
 }
 
 export default StudentRoutine;
+
+
+
+
+// import React, { useState, useEffect } from "react";
+// import Navbar from "../components/StudentNavbar";
+// import { getCookie } from "../utils.js";
+// import axios from "axios";
+// import "../styles/StudentRoutine.css";
+
+// function StudentRoutine() {
+//   const days = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//   ];
+
+//   const periods = [
+//     { start: "7:00 AM", end: "8:00 AM" },
+//     { start: "8:00 AM", end: "9:00 AM" },
+//     { start: "9:00 AM", end: "10:00 AM" },
+//     { start: "10:00 AM", end: "11:00 AM" },
+//     { start: "11:00 AM", end: "12:00 PM" },
+//     { start: "12:00 PM", end: "1:00 PM" },
+//     { start: "1:00 PM", end: "2:00 PM" },
+//   ];
+
+//   const [schedule, setSchedule] = useState({});
+//   const [email, setEmail] = useState('');
+
+//   useEffect(() => {
+//     const fetchRoutine = async () => {
+//       try {
+//         const response = await axios.post('http://localhost:8000/student/routine', { email });
+//         setSchedule(response.data);
+//       } catch (error) {
+//         console.log(error);
+//       }
+//     };
+//     fetchRoutine();
+//   }, [email]);
+
+//   return (
+//     <>
+//       <Navbar />
+//       <div className="container ">
+//         <h1>Student Class Schedule</h1>
+//         <table className="table mt-3">
+//           <thead>
+//             <tr>
+//               <th>Time</th>
+//               {periods.map((period) => (
+//                 <th key={period.start}>{`${period.start} - ${period.end}`}</th>
+//               ))}
+//             </tr>
+//           </thead>
+//           <tbody>
+//             {days.map((day) => (
+//               <tr key={day}>
+//                 <td>{day}</td>
+//                 {periods.map((period) => {
+//                   const subject = schedule[day]?.find(
+//                     (s) =>
+//                       s.start_time === period.start &&
+//                       s.end_time === period.end
+//                   );
+//                   return (
+//                     <td key={`${day}-${period.start}`}>
+//                       {subject ? subject.course : ""}
+//                     </td>
+//                   );
+//                 })}
+//               </tr>
+//             ))}
+//           </tbody>
+//         </table>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default StudentRoutine;
