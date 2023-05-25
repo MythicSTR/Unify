7d6c554b62d973c88c0194c8a3923f45b9054619
Shishir Tamrakar <stamrakar545@gmail.com>
2023-05-26 05:20:12 +0545

    frontend for student's message view

--- frontend/src/components/StudentMessages.js
+++ frontend/src/components/StudentMessages.js
@@ -1,11 +1,63 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import jwtDecode from "jwt-decode";
+
+import "../styles/messages.css";
 import Navbar from "../components/StudentNavbar";
+
+const token = localStorage.getItem('jwtToken')
+const user = jwtDecode(token)
   
 const StudentMessages = () => {
+    const [messages, setMessages] = useState([]);
+    const [feedback, setFeedback] = useState([]);
+    const [replies, setReplies] = useState([]);
+
+    const fetchMessages = async() => {
+        const _response = await axios.post('http://localhost:8000/extract_sfeedback/',{
+        'user_id' : user.user_id
+        })
+        setMessages(_response.data)
+
+        setFeedback(messages.filter(item => !item.hasOwnProperty('feedback_id')));
+
+        setReplies(messages.filter(item => item.hasOwnProperty('feedback_id')));
+
+    }
+
+    useEffect(() => {
+        fetchMessages();
+    })
+    
     return(
         <div>
             <Navbar />
+            <div className="messages-container">
+                <h1>Messages</h1>
+                <hr />
+                <div className="feedback-container">
+                    {feedback.map((feed, index) => {
+                        const corrReplies = replies.filter(reply => feed.id === reply.feedback_id)
+
+                        return (
+                            <div>
+                                <div className="feedback-card">
+                                    <div className="feedback-title">To {feed.email}</div>
+                                    <div className="feedback-message">{feed.comment}</div>
+                                </div>
+                                {replies.map((reply, index) => (
+                                    <div className="reply-card">
+                                        <div className="reply-title">{reply.student}</div>
+                                        <div className="reply-message">{reply.comment}</div>
+                                    </div>
+                                ))}
+                            </div>
+                        )
+                        })}
+                </div>
+            </div>
         </div>
     );
 }
+
 export default StudentMessages;
