f38593e6c554d63510c6553af5ab36785c2a0796
Prajwal-Adhikari <prazolppp@gmail.com>
2023-05-26 06:31:33 +0545

    Classroom deletion done

--- frontend/src/components/Base.js
+++ frontend/src/components/Base.js
@@ -1,118 +1,3 @@
-// import React, { useEffect } from "react";
-// import Navbar from "./StudentNavbar";
-// import "../styles/Base.css";
-// import GetImage from "./GetImage";
-// import { useParams } from "react-router-dom";
-// import { useState } from "react";
-
-// import jwt_decode from "jwt-decode";
-// function Base(props) {
-//     const { course_code } = useParams();
-//     const token = localStorage.getItem("jwtToken");
-//     const user = jwt_decode(token);
-//     const [inputData, setInputData] = useState('');
-//     const [notices, setNotices] = useState([]); //for teacher
-
-//     const handleInputChange = (event) => {
-//         setInputData(event.target.value);
-//     };
-
-//     const handleSubmit = async (event) => {
-//         event.preventDefault();
-
-
-//          // Prepare the data to be sent
-//         const data = {
-//             notice: inputData,
-//             course_code: course_code,
-//             user_id: user.user_id
-//         };
-//         fetch("http://localhost:8000/addclassnotice/", {
-//             method: "POST",
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify(data)
-//         })
-//             .then(response => {
-//                 // Handle the response from the backend API
-//                 if (response.status == 400) {
-//                     window.alert("notice added to the class!");
-//                 } else {
-//                     console.error("Error:", response.statusText);
-//                 }
-//             })
-//             .catch(error => {
-//                 console.error("Error:", error);
-//             });
-//     };
-
-//     const idForTeacherNotices = {
-//         id:sessionStorage.getItem("class_id")
-//     }
-//     useEffect(() => {
-//         fetch("http://localhost:8000/gettnotices/", {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json"
-//           },
-//           body: JSON.stringify(idForTeacherNotices)
-//         })
-//           .then(response => {
-//             if (response.ok) {
-//               return response.json();
-//             } else {
-//               throw new Error("Error fetching notices.");
-//             }
-//           })
-//           .then(data => {
-//             setNotices(data);
-//             console.log(notices);
-//           })
-//           .catch(error => {
-//             console.error("Error:", error);
-//           });
-//       }, []);
-      
-//     return (
-//         <div>
-//             <Navbar />
-//             <div className="main-base-container">
-//                 <h4>Welcome to class {course_code}</h4>
-//             </div>
-//             <GetImage />
-//             <div className="base-notice-section">
-//                 <h3>Notice</h3>
-//                 {!user.isStudent && <div>
-//                     {notices.map((notice, index) => (
-//                         <div key={index}>{notice.notice}</div>
-//                       ))}
-//                   </div>}
-//                 {!user.isStudent &&
-//                     <div className="notice">
-//                         <form onSubmit={handleSubmit}>
-//                             <label htmlFor="inputData">Add a notice:</label>
-//                             <input
-//                                 type="text"
-//                                 id="inputData"
-//                                 className="notice-input-field"
-//                                 name="inputData"
-//                                 value={inputData}
-//                                 onChange={handleInputChange}
-//                                 style={{ width: '50rem', height: '7rem' }}
-//                             />
-//                             <button type="submit" className="Submit-Button-Notice">Submit</button>
-//                         </form>
-//                     </div>}
-
-
-//             </div>
-//         </div>
-//     );
-// }
-
-// export default Base;
-
 import React, { useEffect, useState } from "react";
 import SNavbar from "./StudentNavbar";
 import TNavbar from "./FacultyNavbar";
@@ -132,6 +17,35 @@ function Base(props) {
     setInputData(event.target.value);
   };
 
+  const DeleteClass = () => {
+    const deleteClassroomData = {
+      id:sessionStorage.getItem("class_id")
+    }
+    let decision = window.confirm("Are you sure you want to delete the classroom?");
+    if(decision){
+      fetch("http://localhost:8000/deleteclassroom/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(deleteClassroomData)
+      })
+      
+        .then(response => {
+          if (response.status === 400) {
+            window.alert("Class deleted successfully!");
+            window.location.href = "/faculty/classroom";
+          
+          } else{
+            window.alert("Sorry Couldn't delete the classroom");
+          }
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
+      
+    }
+  }
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -224,8 +138,13 @@ function Base(props) {
         {!user.isStudent && <TNavbar/>}
     <div className="main-base-container">
         <h4>Welcome to class {course_code}</h4>
+        </div>
+        <div className="image-class-delete-btn">
+        <GetImage />
+        <button className="dlt-btn" onClick={DeleteClass}>Delete Classroom</button>
+        </div>
+      <div>
       </div>
-      <GetImage />
       <div className="base-notice-section">
         <h3>Notices</h3>
         {!user.isStudent && (
@@ -268,7 +187,7 @@ function Base(props) {
                 onChange={handleInputChange}
                 style={{ width: "50rem", height: "7rem" }}
               />
-              <button type="submit" className="Submit-Button-Notice">
+              <button type="submit" className="Submit-Button-Notice" onClick={handleSubmit}>
                 Submit
               </button>
             </form>
